2036a3ec777b681a2800e2c9df2ab0951c245450
Ananya Chakraborty Gourab <aborty174@gmail.com>
2026-04-06 22:25:25 +0600

    Initial commit

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layout/MainLayout";
+import HomePage from "../pages/homepage/HomePage";
+import Books from "../pages/books/Books";
+
+export const router=createBrowserRouter([
+  {
+  path:"/",
+  Component: MainLayout ,
+  children:[
+    {
+      index:true,
+      element:<HomePage />
+    },
+    {
+      path:"/books",
+      element:<Books />
+    }
+  ]
+
+},
+{
+  path:"books",
+  element:"books"
+}
+])
